73119345855bb555f0c64fbbac622b41cb7e0253
qujinhao <54793306+qujinhao@users.noreply.github.com>
2019-10-30 22:09:32 +0800

    Update and rename Zazhi1.java to Zazhi（5）读物.java

--- Zazhi（5）读物.java
+++ Zazhi（5）读物.java
@@ -1,5 +1,9 @@
 package bookmanage1;
-
+/**
+*Description:课本习题6.三.1
+*Author:qjh
+*Date:2019-10-30
+*/
 public class Zazhi1 extends Book1 {
     private String zdate;
     public Zazhi1(int pages,int price,String zdate){
