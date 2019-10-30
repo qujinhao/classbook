a9f213d44ee38808da03d903b38661c9e0a70301
qujinhao <54793306+qujinhao@users.noreply.github.com>
2019-10-30 22:07:27 +0800

    Update and rename Keben.java to Keben(2)读物.java

--- Keben(2)读物.java
+++ Keben(2)读物.java
@@ -1,5 +1,9 @@
 package bookmanage1;
-
+/**
+*Description:课本习题6.三.1
+*Author:qjh
+*Date:2019-10-30
+*/
 public class Keben extends Book1 {
     private String duixiang;
     public Keben(int pages,int price,String duixiang){
